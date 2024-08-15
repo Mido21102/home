928c2637756be597dc30f09feb36e0e70d21945b
Mido21102 <167172211+Mido21102@users.noreply.github.com>
2024-08-15 16:41:32 +0300

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const navToggleIcon = document.getElementById('nav-toggle-icon');
-const navMenu = document.getElementById('nav-menu');
-
-navToggleIcon.addEventListener('click', () => {
-  navMenu.classList.toggle('show');
-});
